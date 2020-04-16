48fa2b96e042a806a1b52313fd70fa5f594fcec7
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-04-16 13:27:39 -0700

    recipe batches ✔︎

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,10 +1,35 @@
 #!/usr/bin/python
 
+# def recipe_maker(dictionary_recipe, dictionary_ingredients)
+
+# dictionary_recipe = {ingredient_name: amount_needed}
+# dictionary_ingredients = {ingredient_name: amount_available_to_me}
+
+# ---------
+
+# output the maximum number of whole batches that can be made for the supplied recipe
+
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+	# if recipe is longer than ingredients return 0
+	# keep a count of how many batches we can make
+	# find the smallest ingredient we have in our supply, ingredients
+	# keep subtracting by the recipe amount until we are in the negatives
+	# return the count
+	if len(recipe) > len(ingredients):
+		return 0
 
+	sm_ing_name, sm_ing_value = ("", math.inf)
+	recipe_ing_value = math.inf
+
+	for key, value in ingredients.items():
+		if value < sm_ing_value:
+			sm_ing_name, sm_ing_value = key, value
+			recipe_ing_value = recipe[key]
+
+	return sm_ing_value // recipe_ing_value
+	
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
