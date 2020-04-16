e79530cb12f4a74a1310522969bde97ac436e1db
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-04-15 19:08:01 -0700

    stock_price ✔︎, need to change it later. Difference is right now at -100000

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python
 
+# understand: what is the problem asking me?
+	# wants to find the max difference (in profit) between the stock price in linear time (left to right)
+	# 
+
+ # receive an input a list of stocks
+ # return maximum profit that can made from single buy and sell
+ # buy first before sell
+
+ # `find_max_profit([1050, 270, 1540, 3800, 2])` should return 3530 = 3800 - 270
+ # find maximum difference between the smallest and largest prices in the list of prices
+
 import argparse
 
 def find_max_profit(prices):
-  pass
+	# find the biggest gap between the numbers
+	difference = -10000000
+	for time, price in enumerate(prices):
+		for it_time, it_price in enumerate(prices):
+			if time - it_time > 0 and difference < price - it_price:
+					#print("price:", price, "iteration price:", it_price, "\n\tprice - iteration price =", price-it_price)
+				difference = price - it_price
+	return difference
+
+print(find_max_profit([1050, 270, 1540, 3800, 2]))
+print(find_max_profit([100, 90, 80, 50, 20, 10]))
 
 
 if __name__ == '__main__':
